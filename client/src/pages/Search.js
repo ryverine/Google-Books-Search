4957cfc22e1a9e4f490b76c02438ff27f70ac13b
ryverine <rwcheek@gmail.com>
2019-08-03 12:17:40 -0400

    Created MongoDB collection GoogleBooksList. Books can now be saved to this collection.

---
+++ client/src/pages/Search.js
@@ -0,0 +1,187 @@
+import React, { Component } from "react";
+import DeleteBtn from "../components/DeleteBtn";
+import SaveBtn from "../components/SaveBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+
+import { Input, FormBtn } from "../components/Form";
+
+import { ResultList, ResultItem } from "../components/Results";
+
+class Search extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: "",
+    results: []
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  saveBook = (book) => {
+
+    console.log(
+      "---- SAVING TO LIST -------------" + "\n" +
+      book.title  + "\n" +
+      book.author  + "\n" +
+      book.description  + "\n" +
+      book.link  + "\n" +
+      book.image  + "\n" +
+      "---------------------------------");
+
+    API.saveBook(book)
+      .then(res => {
+        // a way to change "SAVE BOOK" to "IN LIST"?
+      })
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  doGoogleBooksSearch = event =>
+  {
+    event.preventDefault();
+    var searchText = this.state.title.trim();
+    API.googleBooksSearch(searchText).then(res => 
+      {
+        console.log(res.data.items);
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" });
+
+        var resDataItems = res.data.items;
+        var searchResults = [];
+
+        if(resDataItems.length > 0)
+        {
+          for(var i = 0; i < resDataItems.length; i++)
+          {
+            var tmpAuthor = "";
+            var tmpImage = "";
+           
+            if (resDataItems[i].volumeInfo.authors)
+            {
+              tmpAuthor = resDataItems[i].volumeInfo.authors[0];
+            }
+            else
+            {
+              tmpAuthor = "UNKNOWN";
+            }
+
+            if (resDataItems[i].volumeInfo.imageLinks)
+            {
+              tmpImage = resDataItems[i].volumeInfo.imageLinks.thumbnail;
+            }
+            else
+            {
+              tmpImage = "no_thumbnail.png";
+            }
+
+            var tmpBook = {
+              author: tmpAuthor,
+              title: resDataItems[i].volumeInfo.title,
+              description: resDataItems[i].volumeInfo.description,
+              link: resDataItems[i].volumeInfo.infoLink,
+              image: tmpImage
+            };
+
+            searchResults.push(tmpBook);
+
+          }
+        }
+        else
+        {
+          var noResult = {
+            author: "",
+            title: "No results found.",
+            description: "",
+            link: "",
+            image: ""
+          };
+
+          searchResults.push(noResult);
+        }
+
+        this.setState({ 
+          books: res.data, 
+          title: "", 
+          author: "", 
+          synopsis: "",
+          results: searchResults
+         });
+
+      });
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>Find Books via Google</h1>
+              <Link to={"/saved/"}>
+                      <strong>
+                        See books you have saved!
+                      </strong>
+            </Link>
+            </Jumbotron>
+            <form>
+              <Input
+                value={this.state.title}
+                onChange={this.handleInputChange}
+                name="title"
+                placeholder="Title (required)"
+              />
+              <FormBtn
+                disabled={!(this.state.title)}
+                onClick={this.doGoogleBooksSearch}
+              >
+                Search Google Books
+              </FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+          <ResultList>
+                {this.state.results.map(result => (
+                  <ResultItem key={result.link} 
+                    title={result.title}
+                    author={result.author}
+                    description={result.description}
+                    link={result.link}
+                    image={result.image}
+                  >
+                      <SaveBtn onClick={() => this.saveBook(result)} />
+                  </ResultItem>
+                ))}
+          </ResultList>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Search;
